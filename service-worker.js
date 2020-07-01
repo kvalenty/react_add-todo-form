ac81c788ea78801722cbf9ced7c8493e6a2046e5
kvalenty <krushunin89@gmail.com>
2020-07-01 22:51:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.99f70b4fc80f98c6ca8c1031116449c2.js"
 );
 
 self.addEventListener('message', (event) => {
